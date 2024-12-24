ae9fab548e27c317c370208d64fe62fe4e2e3a6b
subhodynamics <subhadeeppathak2013@gmail.com>
2024-12-25 00:16:29 +0530

    Remove unnecessary starter files

--- Starter Files/content.js
+++
@@ -1,7 +0,0 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
